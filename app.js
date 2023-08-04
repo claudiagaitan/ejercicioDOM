928fc9f920f88d207fd0f3cee2150adda4cfe2ba
claudiagaitan <claudiamaritzag@hotmail.com>
2023-08-04 16:58:17 -0500

    Misión completada
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    modified:   app.js
    modified:   styles.css

--- app.js
+++ app.js
@@ -1,28 +1,31 @@
 // Función para agregar una nueva tarea a la lista
 function agregarTarea() {
-  const nuevaTareaInput = ...;
-  const nuevaTareaTexto = ...;
+  const nuevaTareaInput = document.getElementById("nuevaTarea");
+  const nuevaTareaTexto = nuevaTareaInput,value;
 
   if (nuevaTareaTexto !== "") {
-    const listaTareas = ...;
-    const nuevaTareaElemento = ...;
-    nuevaTareaElemento.textContent = ...;
+    const listaTareas = document.getElementById("listaTareas");
+    const nuevaTareaElemento = CreateElement(!li");
+   
+    nuevaTareaElemento.textContent = nuevaTareaTexto;
 
-    const completarBoton = ...;
+    const completarBoton = document.createElement(button");
     completarBoton.textContent = "Completar";
+    completarBoton.classList.add("btn-completar");
     completarBoton.onclick = function () {
       nuevaTareaElemento.classList.toggle("completada");
     };
 
-    const eliminarBoton = ...;
+    const eliminarBoton = document.createElement("button");
     eliminarBoton.textContent = "Eliminar";
+    eliminarBoton.classList.add("btn-eliminar");
     eliminarBoton.onclick = function () {
       listaTareas.removeChild(nuevaTareaElemento);
     };
 
     // nuevaTareaElemento agregar elementos completarBoton y eliminarBoton a la nueva tarea
-
-
+    nuevaTareaElemento.appendChild(completarBoton);
+    nuevaTareaElemento.appendChild(eliminarBoton);
     listaTareas.appendChild(nuevaTareaElemento);
     nuevaTareaInput.value = "";
   }
